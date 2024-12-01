0909518f77b7be8d77b3844af30cf7d7fdb0e761
reasonmin <leeumin7176@gmail.com>
2024-12-01 12:27:53 +0900

    딕셔너리 사용

--- Assets/Scenes/DictionaryTest.cs
+++ Assets/Scenes/DictionaryTest.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Collections;
-using System.Diagnostics.Eventing.Reader;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DictionaryTest : MonoBehaviour
